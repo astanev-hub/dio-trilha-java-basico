db7eabe611ce18ddc882d422b3312cc8dddbc73c
stanev <astanev@hotmail.com>
2025-03-01 16:24:02 -0300

    Inclusão do código de Cadastro de Clientes

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-    }
+        System.out.println(juntar("Alexandre", "Stanev"));
+            }
+        
+            private static String juntar(String string, String string2) {
+                // TODO Auto-generated method stub
+                return string + " " + string2;
+            }
 }
